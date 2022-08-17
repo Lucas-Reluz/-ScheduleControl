31e32890d7f59e8a6e931a5390888fe60c085fbc
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-08-17 18:47:11 -0300

    @Lucas-Reluz [Task: Repaired Patient Model] {17/08/2022 | 18:47}

--- ScheduleControl/src/models/PatientModel.cs
+++ ScheduleControl/src/models/PatientModel.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ScheduleControl.src.models
 {
