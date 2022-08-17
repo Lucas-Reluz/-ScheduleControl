778a8d4f023e02e4aae8c501ca0dd2956d228b45
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-08-17 15:53:51 -0300

    @Lucas-Reluz [Task: Started the project and started to build the models] {17/08/2022 | 15:53}

---
+++ ScheduleControl/src/data/ScheduleControlContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ScheduleControl.src.data
+{
+    /// <summary>
+    /// <para>Summary: Context class, responsible for loading context and defining DbSets </para>
+    /// <para>Created by: Lucas Reluz</para>
+    /// <para>Version: 1.0</para>
+    /// <para>Data: 17/08/2022</para>
+    /// </summary>
+    public class ScheduleControlContext : DbContext
+    {
+        public DbSet<DoctorModel> Doctors { get; set; }
+        public DbSet<PatientModel> Patients { get; set; }
+        public DbSet<QueryModel> Queries { get; set; }
+
+        public ScheduleControlContext(DbContextOptions<ScheduleControlContext> opt) : base(opt)
+        {
+
+        }
+    }
+}
---
+++ ScheduleControl/src/models/DoctorModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ScheduleControl.src.models
+{
+    /// <summary>
+    /// <para>Summary: Mirror class responsible for doctors on the server</para>
+    /// <para>Created by: Matheus Correia</para>
+    /// <para>Version: 1.0</para>
+    /// <para>Data: 17/08/2022</para>
+    /// </summary>
+   [Table("tb_doctors")]
+    public class DoctorModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength()]
+    }
+}
